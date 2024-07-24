e7a9a15cbc24a729e27fe883172dbf3b58e83e06
Malcolm Greaves <mgreaves@nvidia.com>
2024-07-24 17:54:51 -0400

    can call A from B

--- setuptools+pip-example/src/pack-b/tests/test_pack_b.py
+++ setuptools+pip-example/src/pack-b/tests/test_pack_b.py
@@ -4,4 +4,7 @@ from myorg.pack_b import hello_world
 
 def test_package():
     assert hello_world() == "Hello world from package B!"
+
+
+def test_integration():
     assert pack_a.hello_world() == "Hello world from package A!"
